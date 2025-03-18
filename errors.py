e1b8498b0a8b0107b0ce405b0002519d98ea2f26
kl1ps3r <secclestonebrown@gmail.com>
2025-03-18 16:37:15 +0000

    added error analysis

---
+++ errors.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+import main
+
+initial_density = 1
+
+sols = []
+event_densities, event_masses, masses, radii = [], [], [], []
+for i in range(2, 10):
+    sols.append(solve_ivp(main.q, [0,10], [initial_density, 0], dense_output=True, 
+                          events=main.event, rtol=10**(-i), atol=1e-6, max_step=1))
+
+    #print(sols[-1].t_events, sols[-1].y_events)
+
+    event_densities.append(sols[-1].y_events[0][0,0])
+    event_masses.append(sols[-1].y_events[0][0,1])
+
+    #print(sol.y[1])
+    
+    masses.append(sols[-1].y[1])
+    radii.append(sols[-1].t)
+
+
+#print(radii, masses)
+#plt.plot(event_masses)
+for i, (mass, radius) in enumerate(zip(masses, radii)):
+    plt.plot(radius, mass, label=f"1e-{i+2}")
+plt.legend()
+plt.show()
+
+"""
+sol = solve_ivp(main.q, [0,10], [initial_density, 0], dense_output=True, 
+                          events=main.event, rtol=1e-1, atol=1e-3)
+print(type(sol.y[1]))
+plt.plot(sol.t, sol.y[1])
+plt.show()"""
